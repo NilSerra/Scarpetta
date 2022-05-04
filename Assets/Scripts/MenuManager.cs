da22e782d264ca1f7f5a12daa3499e583486f9f5
Arnau Font González <arnaufont.2017@hotmail.com>
2022-05-05 01:47:25 +0300

    message at the beginnning of the game (first time) and changed floooooor

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -9,11 +9,23 @@ public class MenuManager : MonoBehaviour
 
     public Text highestScoreText;
     public Text totalCoinsText;
+
+    
+
+    public GameObject helpMessagePanel;
     void Start() {
         float highestScore = PlayerPrefs.GetInt("HighestScore", 0);
         float totalCoins = PlayerPrefs.GetInt("TotalCoins", 0);
         highestScoreText.text = "HIGHEST SCORE " + highestScore+" m";
         totalCoinsText.text = "Total Coins: " + totalCoins;
+        ShowHelpMessage();
+    }
+
+    private void ShowHelpMessage(){
+        if(SceneManager.GetActiveScene().name == "MenuScreen" && PlayerPrefs.GetInt("FirstTimeOpening", 1)==1){
+            helpMessagePanel.SetActive(true);
+            PlayerPrefs.SetInt("FirstTimeOpening", 0);
+        }
     }
     public void PlayGame(){
         SceneManager.LoadScene("MainScene");
